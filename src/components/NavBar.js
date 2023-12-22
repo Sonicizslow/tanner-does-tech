6ae4765fbce48607a267c51a3a7dfce54b40ee43
Tanner Miller <77452076+Sonicizslow@users.noreply.github.com>
2023-12-21 22:00:05 -0500

    Some more tailwind changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,19 +7,19 @@ const NavBar = () => {
   return (
     <nav className="flex items-center justify-between bg-zinc-500 shadow-md">
       <div className="flex items-center">
-        <img src={logo} alt="Logo" style={{height: '6rem'}} />
+        <img src={logo} alt="Logo" style={{height: '6rem'}} href="#home"/>
       </div>
       <ul className="flex space-x-4">
-        <li className="font-medium hover:bg-gray-700 px-3 py-2 rounded">
+        <li className="text-lg hover:bg-gray-700 px-3 py-2 rounded">
           <a href="#home">Home</a>
         </li>
-        <li className="font-medium hover:bg-gray-700 px-3 py-2 rounded">
+        <li className="text-lg hover:bg-gray-700 px-3 py-2 rounded">
           <a href="#projects">Projects</a>
         </li>
-        <li className="font-medium hover:bg-gray-700 px-3 py-2 rounded">
+        <li className="text-lg hover:bg-gray-700 px-3 py-2 rounded">
           <a href="#about">About me</a>
         </li>
-        <li className="font-medium hover:bg-gray-700 px-3 py-2 rounded">
+        <li className="text-lg hover:bg-gray-700 px-3 py-2 rounded">
           <a href="#contact">Contact</a>
         </li>
       </ul>
