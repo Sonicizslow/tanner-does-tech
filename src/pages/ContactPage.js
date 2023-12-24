6aeb349a0a67ab63c042793e92587cba25688318
Tanner Miller <77452076+Sonicizslow@users.noreply.github.com>
2023-12-23 21:41:20 -0500

    New Contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,48 +1,29 @@
-//ContactPage.js
+// ContactPage.js
 
 function ContactPage() {
   return (
-    <div className="bg-black text-white">
+    <div className="App bg-black text-white text-center py-10">
+      <h1 className="text-4xl font-bold mb-5">Contact Me</h1>
+      
+      <p className="mb-4 text-xl max-w-4xl mx-auto leading-relaxed">
+        I'm always open to discussing new opportunities, collaborations, or just to chat about tech. Feel free to reach out to me through any of the following channels:
+      </p>
 
-      <body class='text-center text-lg'>
-
-        {/* Header Section */}
-        <div class='py-8 sm:py-14'>
-          <h1 class='text-6xl font-bold'>Contact Me</h1>
-          <p class='text-xl mt-4'>Let's get in touch</p>
+      <div className="contact-options">
+        <div className="email mb-3">
+          <h2 className="text-2xl font-semibold">Email</h2>
+          <p><a href="mailto:your-email@example.com" className="hover:text-gray-300">your-email@example.com</a></p>
         </div>
 
-        {/* Contact Form and Details */}
-        <div className='px-8 py-5'>
-          <div className='max-w-lg mx-auto'>
-            <form>
-              <div class='mb-6'>
-                <label for='name' class='block mb-2 text-xl font-medium'>Your Name</label>
-                <input type='text' id='name' class='bg-gray-700 border border-gray-600 text-white text-lg rounded-lg block w-full p-2.5' placeholder='Name'/>
-              </div>
-
-              <div class='mb-6'>
-                <label for='email' class='block mb-2 text-xl font-medium'>Your Email</label>
-                <input type='email' id='email' class='bg-gray-700 border border-gray-600 text-white text-lg rounded-lg block w-full p-2.5' placeholder='Email'/>
-              </div>
-
-              <div class='mb-6'>
-                <label for='message' class='block mb-2 text-xl font-medium'>Your Message</label>
-                <textarea id='message' class='bg-gray-700 border border-gray-600 text-white text-lg rounded-lg block w-full p-2.5' rows='4' placeholder='Message'></textarea>
-              </div>
-
-              <button type='submit' class='text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center'>Submit</button>
-            </form>
-          </div>
+        <div className="social-media">
+          <h2 className="text-2xl font-semibold">Social Media</h2>
+          <p className="social-links">
+            <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300">LinkedIn</a> |
+            <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300"> Twitter</a> |
+            <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300"> GitHub</a>
+          </p>
         </div>
-
-        {/* Optional: Additional Contact Information */}
-        <div className='px-8 py-5'>
-          <p class='text-xl'>Alternatively, feel free to reach out via email at contact@email.com or connect with me on LinkedIn.</p>
-        </div>
-
-      </body>
-
+      </div>
     </div>
   );
 }
