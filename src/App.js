5e02decdeca7ba25fa3aa281b37fddb5c7ed18b3
Tanner Miller <77452076+Sonicizslow@users.noreply.github.com>
2023-12-28 20:05:10 -0500

    more GA changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, userlocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import CookieConsent from './components/CookieConsent';
@@ -8,30 +8,27 @@ import ProjectsPage from './pages/ProjectsPage';
 import AboutPage from './pages/AboutPage'; 
 import ContactPage from './pages/ContactPage';
 import NotFoundPage from './pages/NotFoundPage';
-import { useEffect } from 'react';
 import ReactGA from 'react-ga';
 
-
-function App() {
-
+function PageTracker() {
   const location = useLocation();
 
   useEffect(() => {
-    ReactGA.initialize('G-38J7V7L4P4');
-    // Track the initial pageview
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
-  useEffect(() => {
-    // Track pageview on location change
     ReactGA.pageview(location.pathname + location.search);
   }, [location]);
 
-  usePageTracking();
+  return null; // This component doesn't render anything
+}
 
+function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-38J7V7L4P4');
+    // Track the initial pageview (optional here, as PageTracker will handle subsequent views)
+  }, []);
 
   return (
     <Router>
+      <PageTracker /> {/* Include the PageTracker component inside the Router */}
       <div className="flex flex-col bg-black text-white min-h-screen">
         <NavBar />
         <CookieConsent />
@@ -45,7 +42,6 @@ function App() {
             {/* Define other routes here */}
           </Routes>
         </div>
-
         <Footer />
       </div>
     </Router>
